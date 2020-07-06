7e8eae5d6bdf13c5d59d7ae6b07ebecaf609cbc7
Carlota Gallart <carlota.gallart@gmail.com>
2020-07-06 12:36:18 +0200

    Change product and color in update product html

--- admin-index.php
+++ admin-index.php
@@ -51,8 +51,8 @@
                 <tr>
                   <th scope="col">ID</th>
                   <th scope="col">Name</th>
-                  <th scope="col">Price</th>
-                  <th scope="col">Stock</th>
+                  <th scope="col">Size</th>
+                  <th scope="col">Color</th>
                   <th scope="col">Edit</th>
                 </tr>
               </thead>
