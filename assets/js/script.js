13b0e66cc11f3609d4e0c5a712e9a3b57fa137da
guilherme-carra <gui_gc@hotmail.com>
2020-07-06 15:44:40 +0200

    added code to show products

--- assets/js/script.js
+++ assets/js/script.js
@@ -210,3 +210,45 @@ function checkAnswers(answersArr) {
 
     return results;
 }
+
+/* ---------------------------------------------------
+    INDEX HOME-PAGE
+----------------------------------------------------- */
+
+catalog = [];
+getAllProducts();
+
+function getAllProducts() {
+    var allProductsReq = new XMLHttpRequest();
+    allProductsReq.onload = function() {
+            catalog = JSON.parse(this.responseText);
+            printCatalog();
+    }
+    allProductsReq.open("GET", "get-all-products.php", true);
+    allProductsReq.send();
+}
+
+function printCatalog() {
+    let emptyCol = '<div class="col-sm-6 col-md-4 col-lg-3 pb-4"></div>';
+    let card = '<div class="card h-100"><div>';
+    let img = '<img src="" class="card-img-top" data-toggle="modal" data-target="#product_details">';
+    let cardBody = '<div class="card-body"></div>';
+    let cardTitle = '<h5 class="card-title"></h5>';
+    let cardText = '<p class="card-text"></p>';
+
+    for (let n = 0; n < Object.keys(catalog).length; n++) {
+        if (Object.keys(catalog)[n] != "last_id") {
+            $("#products_list").append(
+                $(emptyCol).append(
+                    $(card).append(
+                        $(img).attr("src", "assets/img/products/"+Object.keys(catalog)[n]+"-"+catalog[n].color[Object.keys(catalog[n].color)[0]]+"-0.jpg"),
+                        $(cardBody).append(
+                            $(cardTitle).text(catalog[n].name),
+                            $(cardText).text(catalog[n].size[Object.keys(catalog[n].size)[0]] + " €/pc")
+                        )
+                    )
+                )
+            )
+        }
+    }
+}
