b2d25b8202265f0ed37804da05099e0081194b97
Carlota Gallart <carlota.gallart@gmail.com>
2020-07-07 12:19:55 +0200

    function search update products and users

--- assets/js/script.js
+++ assets/js/script.js
@@ -213,4 +213,21 @@ function checkAnswers(answersArr) {
 
 //!script admin //
 
-alert("hola");
+// search admins
+$("#edit-user").click(function(){
+    alert("gola");
+})
+$("#search-user-table").on("keyup", function () {
+    const value = $(this).val().toLowerCase();
+    $("#user-table tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+});
+
+// search product
+$("#search-product-table").on("keyup", function () {
+    const value = $(this).val().toLowerCase();
+    $("#table-products tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+})
