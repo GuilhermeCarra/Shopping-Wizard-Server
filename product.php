99eaca4f8563cb85c14591c10eb77a8b4147cf44
Chris <ch.callau@gmail.com>
2020-07-08 04:26:05 +0200

    added jquery to product php for ajax calls

--- product.php
+++ product.php
@@ -293,6 +293,7 @@
         </main>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="./assets/js/script.js"></script>
     <script src="https://kit.fontawesome.com/de217cab6a.js" crossorigin="anonymous"></script>
 </body>
