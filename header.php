579d1f6a0fbde0c06a3455fbc8e1acc743179e20
guilherme-carra <gui_gc@hotmail.com>
2020-07-09 19:37:22 +0200

    fixed header logo link to send to home-page

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     echo
     '<!-- HEADER -->
     <nav class="navbar navbar-light bg-light sticky-top">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="index.php">
                 <img src="assets/img/icons/logo.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
                 Nice Shoes
             </a>
