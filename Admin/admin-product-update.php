f0579b077d3542ac6b9b5f6488de3bf2b7ab353f
guilherme-carra <gui_gc@hotmail.com>
2020-07-09 15:25:26 +0200

    starting to work on product listing

--- Admin/admin-product-update.php
+++ Admin/admin-product-update.php
@@ -13,7 +13,7 @@ var_dump($products);
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>eCommerce</title>
       <link rel="stylesheet" href="..\style-admin.css">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">  <link rel="stylesheet" href="assets/css/style.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     </head>
     <body class="bg-light">
       <div id="inAdminPage" class="d-none"></div>
@@ -62,6 +62,15 @@ var_dump($products);
                   <td><a href="../Admin/edit-product.php"><button type="button" class="btn btn-dark" id="edit-product">Edit</button></a>
                     <button type="button" class="btn btn-danger">X</button></td>
                 </tr>
+                <?php
+                foreach($products as $id => $product){
+                  if($id != 'last_id'){
+                    echo $id. "<br>";
+                    var_dump($product);
+                    echo "<br>";
+                  }
+                }
+                ?>
               </tbody>
             </table>
           </div>
